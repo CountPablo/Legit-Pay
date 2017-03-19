0fbfd1942d43736a9e6d85b4bb69df4e784fd7ab
Milan Singh <milanprsingh@gmail.com>
2017-03-18 23:57:45 -0400

    Added PostActivity, finished showroom_item

--- app/src/main/java/com/example/pablo/legitpay/ShowroomActivity.java
+++ app/src/main/java/com/example/pablo/legitpay/ShowroomActivity.java
@@ -1,13 +1,21 @@
 package com.example.pablo.legitpay;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ShowroomActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showroom);
+    }
+
+    public void openPost(View view) {
+        Intent intent = new Intent(this, profileScreen.class);
+        startActivity(intent);
     }
 }
